55f5afc95c7a726dbbfd1e5d9f5631da925418d5
rafnakb <rbnakati@hotmail.com>
2023-05-22 23:27:16 -0300

    Review anottations to run modules

--- task-service/src/main/java/com/nyuro/taskservice/TaskServiceApplication.java
+++ task-service/src/main/java/com/nyuro/taskservice/TaskServiceApplication.java
@@ -3,9 +3,13 @@ package com.nyuro.taskservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.nyuro.domain")
+@EntityScan("com.nyuro.domain")
+@ComponentScan({"com.nyuro.domain"})
 @SpringBootApplication
-@EntityScan("com.nyuro.domain.*")
 public class TaskServiceApplication {
 
 	public static void main(String[] args) {
